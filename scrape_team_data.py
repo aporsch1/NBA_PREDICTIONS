b3dc5a2359ff6d88d23be13d84a5580277bd4d4a
Abe Porschet <abe.porschet@icloud.com>
2024-02-25 21:48:44 -0500

    RAWR

--- scrape_team_data.py
+++ scrape_team_data.py
@@ -1,1 +1,6 @@
 #TODO: use selenium or another scraping procedure to generate (and later maybe use cron to update) tables of team aggregated data for each NBA team.
+import pandas as pd
+import numpy as np
+import plotly.express as px
+import BeautifulSoup
+
